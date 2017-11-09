b209733eb52a503c38368e90e2d29c1c28e0d149
Lyubomir Marinov <marinov@Lyubomirs-MacBook-Air.local>
2017-11-09 08:39:05 +0200

    Added the XCode project

---
+++ Weather-or-not/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Weather-or-not
+//
+//  Created by Lyubomir Marinov on 9.11.17.
+//  Copyright © 2017 Lyubomir Marinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let csvfile = Bundle.main.path(forResource: "demo", ofType: "csv") else { return }
+        do {
+            let csvpath = URL(fileURLWithPath: csvfile)
+            let csvdata = try Data(contentsOf: csvpath)
+            
+            let csvstring = String(data: csvdata, encoding: .utf8)
+            
+            label.text = csvstring
+        } catch {
+            print("awedeebawe")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
